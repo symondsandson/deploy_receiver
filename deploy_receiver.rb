055d84c0353eeb5100ec2e9a6db6feebe05a9d2a
Josh Symonds <veraticus@gmail.com>
2015-09-09 16:55:15 -0500

    Update deploy_receiver

--- deploy_receiver.rb
+++ deploy_receiver.rb
@@ -9,29 +9,50 @@ get '/' do
   Oj.dump(json)
 end
 
-post '/github' do
+post '/deploy' do
   request.body.rewind
   payload_body = request.body.read
-  verify_signature(payload_body)
-  push = Oj.load(params['payload'])
+  verify_signature(payload_body, request.env['HTTP_X_SIGNATURE'])
 
+  push = Oj.load(params['payload'])
   puts "Payload: #{push.inspect}"
 
-  sender = push['sender']['login']
-  application = push['repository']['name']
-  environment = push['ref'].split('/').last
-  send_consul_deploy(application, environment, sender, 'GitHub')
+  send_consul_deploy(
+    sender: push['sender'],
+    application: push['application'],
+    environment: push['environment'],
+    source: push['source']
+  )
 
   json = {ok: true}
   Oj.dump(json)
 end
 
-def verify_signature(payload_body)
-  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
-  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+post '/github' do
+  request.body.rewind
+  payload_body = request.body.read
+  verify_signature(payload_body, request.env['HTTP_X_HUB_SIGNATURE'])
+
+  push = Oj.load(params['payload'])
+  puts "Payload: #{push.inspect}"
+
+  send_consul_deploy(
+    sender: push['sender']['login'],
+    application: push['repository']['name'],
+    environment: push['ref'].split('/').last,
+    source: 'GitHub'
+  )
+
+  json = {ok: true}
+  Oj.dump(json)
 end
 
-def send_consul_deploy(application, environment, sender, source)
+def verify_signature(payload_body, signature)
+  calc = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(calc, signature)
+end
+
+def send_consul_deploy(application:, environment:, sender:, source:)
   application, environment, sender, source = [Shellwords.escape(application), Shellwords.escape(environment), Shellwords.escape(sender), Shellwords.escape(source)]
 
   deploy = "#{application}-#{environment}-deploy"
